039537b7e3c4e25f5be60697126751ac17da431a
SHND70 <57438244+SHND70@users.noreply.github.com>
2021-07-16 21:04:00 +0900

    mul

--- src/index.js
+++ src/index.js
@@ -145,7 +145,11 @@
 	/* 演習1 ここまで */
 
 	/* TODO: 演習2 以降は下記に自由に記入 */
-
+	//掛け算用の関数 mul
+	function mul(){
+		eqlNum.value = parseFloat(midNum[0].value) * parseFloat(midNum[1].value);
+	}
+	
 	/* 演習の編集範囲 おわり */
 
 	// ac を押したときの処理
