9e7ebdd4c3568240641c3a8b8ab1057600b5b285
SHND70 <57438244+SHND70@users.noreply.github.com>
2021-07-16 21:06:58 +0900

    add coment

--- src/index.js
+++ src/index.js
@@ -145,11 +145,11 @@
 	/* 演習1 ここまで */
 
 	/* TODO: 演習2 以降は下記に自由に記入 */
-	//掛け算用の関数 mul
+	//掛け算用の関数 mul(VSCode上で履歴残ってるとpullが反映されない)
 	function mul(){
 		eqlNum.value = parseFloat(midNum[0].value) * parseFloat(midNum[1].value);
 	}
-	
+
 	/* 演習の編集範囲 おわり */
 
 	// ac を押したときの処理
